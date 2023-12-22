f96921c580410bf18c7c180040f62f9303cc8ec3
YuliiaKush <128642386+YuliiaKush@users.noreply.github.com>
2023-12-22 15:20:04 +0200

    final update

---
+++ routes/api/contacts-router.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import contactsController from "../../controllers/contacts-controller.js";
+
+import { isEmptyBody } from "../../middlewares/index.js";
+
+const contactsRouter = express.Router();
+
+contactsRouter.get("/", contactsController.getAll);
+
+contactsRouter.get("/:id", contactsController.getById);
+
+contactsRouter.post("/", isEmptyBody, contactsController.add);
+
+contactsRouter.delete("/:id", contactsController.deleteById);
+
+contactsRouter.put("/:id", isEmptyBody, contactsController.updateById);
+
+export default contactsRouter;
